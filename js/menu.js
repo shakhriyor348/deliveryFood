c647a680482ae168ad6ada803e158bcc52356a73
shakhriyor348 <62033235+shakhriyor348@users.noreply.github.com>
2021-11-08 01:10:07 +0300

    first

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,59 @@
-const restoraunt = 'food-band'
+const restoraunt = JSON.parse(localStorage.getItem('restaurant'))
 
-const renderItems = (data) => {
-    console.log(data);
+const cardsMenu = document.querySelector('.cards-menu')
+
+const changeTitle = (restaurant) => {
+    const restaurantTitle = document.querySelector('.restaurant-title'),
+    rating = document.querySelector('.rating'),
+    price = document.querySelector('.price'),
+    category = document.querySelector('.category')
+
+    console.log(restaurant);
+
+    restaurantTitle.textContent = restaurant.name
+    rating.textContent = restaurant.stars
+    price.textContent = restaurant.price
+    category.textContent = restaurant.kitchen
 }
 
-fetch(`./db/${restoraunt}.json`)
+const renderItems = (data) => {
+    data.forEach(({ description, id, image, name, price }) => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+
+        card.innerHTML = `
+        <img src="${image}" alt="${name}" class="card-image" />
+        <div class="card-text">
+            <div class="card-heading">
+                <h3 class="card-title card-title-reg">${name}</h3>
+            </div>
+            <!-- /.card-heading -->
+            <div class="card-info">
+                <div class="ingredients">
+                    ${description}
+                </div>
+            </div>
+            <!-- /.card-info -->
+            <div class="card-buttons">
+                <button class="button button-primary button-add-cart">
+                    <span class="button-card-text">В корзину</span>
+                    <span class="button-cart-svg"></span>
+                </button>
+                <strong class="card-price-bold">${price} ₽</strong>
+            </div>
+        </div>
+        `
+
+        cardsMenu.append(card)
+    });
+}
+
+if(localStorage.getItem('restaurant')) {
+    changeTitle(restoraunt)
+    fetch(`./db/${restoraunt.products}`)
     .then((res) => res.json())
     .then((data) => renderItems(data))
+}else {
+    window.location.href = '/'
+}
+
